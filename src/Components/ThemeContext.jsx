9f77e475e442d1e77153bd60610807893d4d940e
daffa901 <rinaldi23ti@mahasiswa.pcr.ac.id>
2025-06-22 14:42:28 +0700

    dappe

---
+++ src/Components/ThemeContext.jsx
@@ -0,0 +1,23 @@
+// src/contexts/ThemeContext.jsx
+import { createContext, useContext, useState, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-mode", darkMode);
+    document.body.classList.toggle("light-mode", !darkMode);
+  }, [darkMode]);
+
+  const toggleTheme = () => setDarkMode((prev) => !prev);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
